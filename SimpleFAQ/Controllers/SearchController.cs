7f6be4e74d28e4efc89df98db770372a02c35224
friedr1c3 <joshua.j.h@me.com>
2013-01-08 16:56:54 +1100

    Added Dapper SqlBuilder class. Added page helpers from SEDE.

--- SimpleFAQ/Controllers/SearchController.cs
+++ SimpleFAQ/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,9 @@ using System.Web.Mvc;
 
 namespace SimpleFAQ.Controllers
 {
+	using Core.Helpers;
+	using Models;
+
 	/// <summary>
 	/// Search controller.
 	/// </summary>
@@ -18,11 +22,31 @@ namespace SimpleFAQ.Controllers
 		/// GET: /search/index
 		/// </summary>
 		/// <returns></returns>
-		public ActionResult Index(string input)
+		public ActionResult Index(string input, int? page, int? pageSize)
         {
 			ViewBag.Input = input;
 
-            return View();
+			page = Math.Max(page ?? 1, 1); // Adjust.
+			pageSize = Math.Max(Math.Min(pageSize ?? 50, 100), 10); // Adjust.
+
+			int start = ((page.Value - 1) * pageSize.Value) + 1;
+			int finish = page.Value * pageSize.Value;
+			var builder = new SqlBuilder();
+			SqlBuilder.Template pager = null, counter = null;
+
+			pager = builder.AddTemplate("SELECT * FROM (SELECT /**select**/, ROW_NUMBER() OVER (/**orderby**/) AS RowNumber FROM Questions Q /**where**/) AS Results WHERE RowNumber BETWEEN @start AND @finish ORDER BY RowNumber", 
+				new { start = start, finish = finish });
+
+			counter = builder.AddTemplate("SELECT COUNT(*) FROM Questions /**where**/");
+
+			builder.Select("Q.*");
+			builder.OrderBy("Q.Time DESC");
+			builder.Where("Q.Title LIKE @searchInput OR Q.Answer LIKE @searchInput", new { searchInput = "%" + input + "%" });
+
+			var results = Current.DB.QueryMultiple(pager.RawSql, pager.Parameters).Read<Question>();
+			var totals = Current.DB.Query<int>(counter.RawSql, counter.Parameters);
+
+			return View(new PagedList<Question>(results, page.Value, pageSize.Value, false, 0));
 		}
 
 		#endregion
